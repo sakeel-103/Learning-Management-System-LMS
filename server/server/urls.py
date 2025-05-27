c57328080671573dd03d7163a2bfb977866caccd
Shrinivas S Patil <9036063869shri@gmail.com>
2025-05-27 12:57:44 +0530

    new chages

--- server/server/urls.py
+++ server/server/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('accounts.urls')),
-    path('api/',include('course_class.urls'))
+    path('api/v1/course_class/',include('course_class.urls'))
 ]
