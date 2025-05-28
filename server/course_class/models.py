6fa8de2205bfc0b63c8379b42c13430db70328be
Shrinivas S Patil <9036063869shri@gmail.com>
2025-05-28 12:15:26 +0530

    Made some model changes

--- server/course_class/models.py
+++ server/course_class/models.py
@@ -5,13 +5,13 @@ from django.db import models
 from django.db import models
 
 class Course(models.Model):
-    title = models.CharField(max_length=255)
-    category = models.CharField(max_length=100)
-    level = models.CharField(max_length=100)
-    duration = models.CharField(max_length=50)
-    instructor = models.CharField(max_length=100)
-    description = models.TextField()
-    prerequisites = models.JSONField(default=list, blank=True)  # or use TextField with JSON string
+    Title = models.CharField(max_length=255)
+    Category = models.CharField(max_length=100)
+    Level = models.CharField(max_length=100)
+    Duration = models.CharField(max_length=50)
+    Instructor = models.CharField(max_length=100)
+    Course_Description = models.TextField()
+    
 
     def __str__(self):
         return self.title
