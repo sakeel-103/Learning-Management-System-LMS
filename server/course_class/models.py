f890b6e50771f062adc3207eb67441c61f78edf8
Shrinivas S Patil <9036063869shri@gmail.com>
2025-05-28 12:23:18 +0530

    new chages in models.py

--- server/course_class/models.py
+++ server/course_class/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 
-# Create your models here.
-# course_class/models.py
-from django.db import models
-
 class Course(models.Model):
-    Title = models.CharField(max_length=255)
-    Category = models.CharField(max_length=100)
-    Level = models.CharField(max_length=100)
-    Duration = models.CharField(max_length=50)
-    Instructor = models.CharField(max_length=100)
-    Course_Description = models.TextField()
-    
+    title = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+    duration = models.CharField(max_length=50)
+    instructor = models.CharField(max_length=100)
+    course_description = models.TextField()
 
     def __str__(self):
         return self.title
